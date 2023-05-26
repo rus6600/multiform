65af285d6fa91e2824f926945a7e997709cb799e
Koshmukhanov <rkoshmukhanov@one.kz>
2023-05-26 17:21:06 +0600

    add prettier

--- src/components/ui/Fonts.ts
+++ src/components/ui/Fonts.ts
@@ -1,16 +1,15 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
-import UbuntuRegular from '../../assets/fonts/UbuntuRegular.ttf';
-import UbuntuBold from '../../assets/fonts/UbuntuBold.ttf';
-import UbuntuMedium from '../../assets/fonts/UbuntuMedium.ttf';
+import UbuntuRegular from '../../assets/fonts/UbuntuRegular.ttf'
+import UbuntuBold from '../../assets/fonts/UbuntuBold.ttf'
+import UbuntuMedium from '../../assets/fonts/UbuntuMedium.ttf'
 
 export const Font = createGlobalStyle`
   body {
     font-family: 'Roboto Condensed', sans-serif;
   }
-@font-face {
-  font-family: 'Ubuntu';
-  src: url(${UbuntuBold}) , url(${UbuntuRegular}),url(${UbuntuMedium}) 
-}
-`;
-
+    @font-face {
+      font-family: 'Ubuntu';
+      src: url(${UbuntuBold}) , url(${UbuntuRegular}),url(${UbuntuMedium}) 
+    }
+`
